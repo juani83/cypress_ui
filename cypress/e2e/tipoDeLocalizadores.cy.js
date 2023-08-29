79568b2b9aff038fe39531e220cc6fce1c9c7824
juani83 <juanbecerra83@gmail.com>
2023-08-29 15:39:20 -0300

    Encontrando elementos

--- cypress/e2e/tipoDeLocalizadores.cy.js
+++ cypress/e2e/tipoDeLocalizadores.cy.js
@@ -21,4 +21,16 @@ describe('Tipos de localizadores', () => {
         cy.get('.btn.btn-outline-primary.float-right');
     });
 
+    it('Usando contains', () => {
+        cy.contains('Book this room');
+        cy.contains('.btn.btn-outline-primary.float-right', 'Submit');
+        cy.contains('.btn.btn-outline-primary.float-right', 'Book this room');
+    });
+
+    it('Usando parent', () => {
+        cy.get('#name').parent();
+        cy.get('#name').parents().find('form');
+        cy.get('form').find('input');
+    });
+
 });
